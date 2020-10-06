22a6db2a5089366221a2c80f1c3ed28ba59f9b0e
Gioele Zardini <gzardini@student.ethz.ch>
2020-10-06 21:58:08 +0200

    added a bunch of yaml

---
+++ ral2020.mcdplib/csv_catalogues/transformCSVtoYaml.py
@@ -0,0 +1,26 @@
+import csv
+import yaml
+
+def transformation(fileName):
+    with open(fileName) as f:
+        reader = csv.reader(f, delimiter=',')
+        target_file = open("amod_micromobility.dpc.yaml", 'w')
+        data = list(reader)
+        target_file.write("implementations:\n")
+        for index in range(len(data)):
+                target_file.write("  "+data[index][0]+": \n")
+                target_file.write("    f_max:\n")
+                target_file.write("    " + "- " + "\""+ data[index][1] + " dimensionless\" \n")
+                target_file.write("    r_min:\n")
+                target_file.write("    " + "- " + "\"" + data[index][2] + " Nat\" \n")
+                target_file.write("    " + "- " + "\"" + data[index][3] + " Nat\" \n")
+                target_file.write("    " + "- " + "\"" + data[index][4] + " miles/hour\" \n")
+                target_file.write("    " + "- " + "\"" + data[index][5] + " miles/hour\" \n")
+                target_file.write("    " + "- " + "\"" + data[index][6] + " s\" \n")
+                target_file.write("    " + "- " + "\"" + data[index][7] + " meter/s\" \n")
+                target_file.write("    " + "- " + "\"" + data[index][8] + " meter/s\" \n")
+                target_file.write("    " + "- " + "\"" + data[index][9] + " Nat\" \n")
+                target_file.write("    " + "- " + "\"" + data[index][10] + " kg/s\" \n")
+
+
+transformation("amod.csv")
--- ral2020.mcdplib/generate_sensing_curves.py
+++ ral2020.mcdplib/generate_sensing_curves.py
@@ -6,6 +6,7 @@ class curve:
         self.sen = []
         self.fn = []
         self.fp = []
+        #self.acc = []
 
 def get_curves(fileName):
     sensor_names = []
@@ -19,27 +20,48 @@ def get_curves(fileName):
             curves = sensors_file[sensor]
             fn = curves["fn"]
             fp = curves["fp"]
+            #acc = curves["acc"]
             s = curve()
             s.sen = sensor
             s.fn = fn
             s.fp = fp
+            #s.acc = acc
             all_curves.append(s)
-        #print(sensor_names)
     return all_curves
+
+def compare_arrays(a,b):
+    " assuming same size"
+    assert len(a)==len(b)
+    pairwise_comp = []
+    for i in range(len(a)):
+        is_smaller = (a[i]<= b[i])
+        pairwise_comp.append(is_smaller)
+    if np.all(pairwise_comp):
+        ret = 1
+    else:
+        ret = 0
+    return ret
 
 def compare_curves(array_curves):
     fn_order = np.zeros((len(array_curves), len(array_curves)))
     fp_order = np.zeros((len(array_curves), len(array_curves)))
+    #acc_order = np.zeros((len(array_curves), len(array_curves)))
 
     for id_s, s in enumerate(array_curves):
         for id_t, t in enumerate(array_curves):
-            order_fn = (s.fn <= t.fn)
-            order_fp = (s.fp <= t.fp)
-            if np.all(order_fn):
-                fn_order[id_s][id_t] = 1
-            if np.all(order_fp):
-                fp_order[id_s][id_t] = 1
-    return fn_order, fp_order
+            if (id_s != id_t):
+                if np.all(s.fn <= t.fn):
+                    fn_order[id_s][id_t] = 1
+                if np.all(s.fp <= t.fp):
+                    fp_order[id_s][id_t] = 1
+                if compare_arrays(s.fn, t.fn):
+                    fn_order[id_s][id_t] = 1
+                if compare_arrays(s.fp, t.fp):
+                    fp_order[id_s][id_t] = 1
+
+                #if np.all(order_acc):
+                #    acc_order[id_s][id_t] = 1
+    return fn_order, fp_order #, acc_order
 
 def produce_poset_fn(fn_order, array_curves):
     poset_fn = open("poset_fn.mcdp_poset", 'w')
@@ -65,12 +87,26 @@ def produce_poset_fp(fp_order, array_curves):
             if (i != j):
                 if (fp_order[i][j]):
                     poset_fp.write(array_curves[i].sen + " <= " + array_curves[j].sen + "\n")
+                if (fp_order[i][j] == fp_order[j][i]):
+                    print (array_curves[i].sen, array_curves[j].sen, " ===")
     poset_fp.write("}")
 
-
+def produce_poset_acc(acc_order, array_curves):
+    poset_acc = open("poset_acc.mcdp_poset", 'w')
+    poset_acc.write("poset {\n")
+    for a in array_curves:
+        poset_acc.write(a.sen + ' ')
+    poset_acc.write("\n")
+    for i in range(len(acc_order)):
+        for j in range(len(acc_order)):
+            if (i != j):
+                if (acc_order[i][j]):
+                    poset_acc.write(array_curves[i].sen + " <= " + array_curves[j].sen + "\n")
+    poset_acc.write("}")
 
 curves_here = get_curves('curves.yaml')
 fn, fp = compare_curves(curves_here)
 produce_poset_fn(fn, curves_here)
 produce_poset_fp(fp, curves_here)
+#produce_poset_acc(acc, curves_here)
 
