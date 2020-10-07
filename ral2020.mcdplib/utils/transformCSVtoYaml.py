b0c1bc0842658784cd2c0b2384f841989b6f7e2e
Gioele Zardini <gzardini@student.ethz.ch>
2020-10-07 09:11:08 +0200

    added control yaml

--- ral2020.mcdplib/utils/transformCSVtoYaml.py
+++ ral2020.mcdplib/utils/transformCSVtoYaml.py
@@ -1,7 +1,8 @@
 import csv
 import yaml
+import numpy as np
 
-def transformation(fileName):
+def transformationAmod(fileName):
     with open(fileName) as f:
         reader = csv.reader(f, delimiter=',')
         target_file = open("amod_micromobility.dpc.yaml", 'w')
@@ -22,5 +23,24 @@ def transformation(fileName):
                 target_file.write("    " + "- " + "\"" + data[index][9] + " Nat\" \n")
                 target_file.write("    " + "- " + "\"" + data[index][10] + " kg/s\" \n")
 
+def transformationLQG(fileName):
+    with open(fileName) as f:
+        reader = csv.reader(f, delimiter=',')
+        target_file = open("lane_control.dpc.yaml", 'w')
+        data = list(reader)
+        target_file.write("implementations:\n")
+        for index in range(len(data)):
+                target_file.write("  "+data[index][0]+": \n")
+                target_file.write("    f_max:\n")
+                target_file.write("    " + "- " + "\""+ data[index][1] + " dimensionless\" \n")
+                target_file.write("    r_min:\n")
+                target_file.write("    " + "- " + "\"" + data[index][2] + " dimensionless\" \n")
+                target_file.write("    " + "- " + "\"" + data[index][3] + " dimensionless\" \n")
+                target_file.write("    " + "- " + "\"" + data[index][4] + " s\" \n")
+                target_file.write("    " + "- " + "\"" + data[index][5] + " dimensionless\" \n")
+                freq = 1.0/float(data[index][4])
+                target_file.write("    " + "- " + "\"" + str(freq) + " Hz\" \n")
 
-transformation("amod.csv")
+
+#transformationAmod("amod.csv")
+transformationLQG("cat_lqg.csv")
